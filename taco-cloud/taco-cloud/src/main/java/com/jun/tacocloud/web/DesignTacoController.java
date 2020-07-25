0ad77be9290c539e2303197a9e0b6300b88a79dd
khj68 <maga40@naver.com>
2020-07-26 00:08:54 +0900

    Make design and orders

---
+++ taco-cloud/taco-cloud/src/main/java/com/jun/tacocloud/web/DesignTacoController.java
@@ -0,0 +1,64 @@
+package com.jun.tacocloud.web;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+import lombok.extern.slf4j.Slf4j;
+import com.jun.tacocloud.Taco;
+import com.jun.tacocloud.Ingredient;
+import com.jun.tacocloud.Ingredient.Type;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+    
+    @GetMapping
+    public String showDesignForm(final Model model) {
+        final List<Ingredient> ingredients = Arrays.asList(
+            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+            new Ingredient("GRBF", "Groud Beef", Type.PROTEIN),
+            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+            new Ingredient("CHED", "Cheddar", Type.CHEESE),
+            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+            new Ingredient("SLSA", "Salsa", Type.SAUCE),
+            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+
+        final Type[] types = Ingredient.Type.values();
+        for (final Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                filterByType(ingredients, type));
+        }
+
+        model.addAttribute("taco", new Taco());
+
+        return "design";
+    }
+
+    @PostMapping
+    public String processDesing(Taco design) {
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(
+        final List<Ingredient> ingredients, final Type type) {
+            return ingredients
+                    .stream()
+                    .filter(x -> x.getType().equals(type))
+                    .collect(Collectors.toList());
+        
+        }
+}
