d8aad9508494b736c375b260472c3260532ec158
khj68 <maga40@naver.com>
2020-07-26 12:02:39 +0900

    Add constraints using hibernate and javax

--- taco-cloud/taco-cloud/src/main/java/com/jun/tacocloud/web/OrderController.java
+++ taco-cloud/taco-cloud/src/main/java/com/jun/tacocloud/web/OrderController.java
@@ -2,11 +2,15 @@ package com.jun.tacocloud.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.Valid;
+
 import com.jun.tacocloud.Order;
 
 @Slf4j
@@ -21,7 +25,10 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
